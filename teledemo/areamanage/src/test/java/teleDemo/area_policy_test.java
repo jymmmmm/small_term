3371a66647110f099670f8cd51ebc009207f070a
thhyj <276897571@qq.com>
2022-08-25 17:01:25 +0800

    add test

---
+++ teledemo/areamanage/src/test/java/teleDemo/area_policy_test.java
@@ -0,0 +1,22 @@
+package teleDemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import teleDemo.util.area_policy;
+
+/**
+ * @Project Name:MSHD
+ * @File Name: area_policy_test
+ * @Description: 测试area_policy逻辑，。。。。。。
+ * @ HISTORY：
+ *    Created   2022.8.25  HeYujie
+ */
+
+public class area_policy_test {
+    @Test
+    public void testJudgeLevel() {
+        Assertions.assertEquals("a", area_policy.judge_level(1000, 0));
+        Assertions.assertEquals("c", area_policy.judge_level(0, 0));
+        Assertions.assertEquals("b", area_policy.judge_level(0, 50));
+    }
+}
