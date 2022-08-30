f341137118ca55dd91a4ec265eb6057681230295
jym <1416320489@qq.com>
2022-08-30 15:48:50 +0800

    修改test

--- teledemo/areamanage/src/test/java/teleDemo/service_test/tableService_test.java
+++ teledemo/areamanage/src/test/java/teleDemo/service_test/tableService_test.java
@@ -3,7 +3,6 @@ package teleDemo.service_test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
